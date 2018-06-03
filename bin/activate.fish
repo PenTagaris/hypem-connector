49c12ca1ca747518d987fc5368c1b382097a9ebf
jstnchristian <jstnchristian@gmail.com>
2018-06-03 10:31:51 -0600

    Add stuff

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/justin/Dropbox/Code/hypem-connector"
+set -gx VIRTUAL_ENV "/home/justin/Dropbox/Code/python/hypem-connector"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
