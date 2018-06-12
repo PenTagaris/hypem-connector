84d4dd48dafb2abd4a22a5303b4d0b18215c06bb
jstnchristian <jstnchristian@gmail.com>
2018-06-12 17:47:50 -0600

    Added some gitignore action, changed email-parser to read all files in s3-email

--- email-parser.py
+++ email-parser.py
@@ -1,27 +1,34 @@
 import re
+from os import listdir
 from bs4 import BeautifulSoup
 from email import message_from_file
 from email.policy import default
 import json
 
-#import the email
-email = message_from_file(open('s3-email/vbd7s83v7eg9apr0peh9ebqi8ujfh4sepogr7381', 'r'), policy=default)
-
 #build our regex
 regex = re.compile(r'(.*)\s-\s(.*)')
 
-#pretty dictionary thing
-d = {}
+#get all the email files in the directory
+#this loop will go away eventually. Right now, I'm using it for testing
+for i in listdir('s3-email'):
 
-#do we have html?
-email_html = email.get_body(preferencelist='html')
-if email_html is not None:
-    soup = BeautifulSoup(email_html.get_content(), 'html.parser')
-    for links in soup.find_all('a'):
-        mo = regex.findall(links.get_text())
-        if len(mo):
-            d[mo[0][0]] = mo[0][1]
+    #import the email
+    email = message_from_file(open('s3-email/{}'.format(i), 'r'), 
+            policy=default)
 
-    with open('gmusic.json', 'w', encoding='utf-8') as json_file:
-        json.dump(d, json_file, ensure_ascii=False)
+    #instantiate the dictionary
+    d = {}
+
+    #do we have html?
+    email_html = email.get_body(preferencelist='html')
+    if email_html is not None:
+        soup = BeautifulSoup(email_html.get_content(), 'html.parser')
+        for links in soup.find_all('a'):
+            mo = regex.findall(links.get_text())
+            if len(mo):
+                d[mo[0][0]] = mo[0][1]
+
+        #write the json
+        with open('json/{}.json'.format(i), 'w', encoding='utf-8') as json_file:
+            json.dump(d, json_file, ensure_ascii=False)
 
